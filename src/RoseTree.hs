93d5452efa4c109c60cece2017c52f36a42b99bf
Diego de Rochebouet <ddegrimaudetderocheb@itba.edu.ar>
2019-07-06 19:14:39 -0300

    Changed minimax to use function to get next nodes
    Thee function does not know fo the underling structure where it's being
    called

--- src/RoseTree.hs
+++ src/RoseTree.hs
@@ -55,7 +55,7 @@ freeTree = Node 1
             ]
 
 -- Move: score, indice de las movidas.
-data MovePath = MovePath { score::Int,
+data MovePath = MovePath { pathScore::Int,
                            nodesExplored::Int,
                            scoreSeen::[Int],
                            indexes::[Int]
@@ -67,35 +67,46 @@ data AlphaBeta = AlphaBeta {
 }
 
 data TreeInfo a = TreeInfo { scoreF::(a -> Int),
+                             nextNodesF::(a -> [a]),
                              level::Int,
                              isMax::Bool --  function to get score, Level, isMax
 }
 
-minimax :: RoseTree a -> TreeInfo a -> Int -> AlphaBeta -> MovePath -- Tree, treeInfo, indexMove
-minimax (Node a []) (TreeInfo scoreF _ _) indexM ab = MovePath (scoreF a) 0 [(scoreF a)] []
-minimax (Node a _) (TreeInfo scoreF 0 _) indexM ab = MovePath (scoreF a) 0 [(scoreF a)] []
-minimax (Node a (r:rs)) treeInfo indexM ab = minimaxHorizontal (nextLevelMinimax r treeInfo 0 ab) rs treeInfo 1 ab
 
-minimaxHorizontal :: MovePath -> [RoseTree a] -> TreeInfo a -> Int -> AlphaBeta -> MovePath
+getRoseTreeScore :: RoseTree b -> b
+getRoseTreeScore (Node b rs) = b
+
+getRoseNextNodes :: RoseTree b -> [RoseTree b]
+getRoseNextNodes (Node b rs) = rs
+
+minimax :: a -> TreeInfo a -> Int -> AlphaBeta -> MovePath -- Tree, treeInfo, indexMove
+minimax node treeInfo indexM ab = minimaxAux node ((nextNodesF treeInfo) node) treeInfo indexM ab
+
+minimaxAux :: a -> [a] -> TreeInfo a -> Int -> AlphaBeta -> MovePath
+minimaxAux node (r:rs) (TreeInfo scoreF _ 0 _) indexM ab = MovePath (scoreF node) 0 [(scoreF node)] []
+minimaxAux node [] (TreeInfo scoreF _ _ _) indexM ab = MovePath (scoreF node) 0 [(scoreF node)] []
+minimaxAux node (r:rs) treeInfo indexM ab = minimaxHorizontal (nextLevelMinimax r treeInfo 0 ab) rs treeInfo 1 ab
+
+minimaxHorizontal :: MovePath -> [a] -> TreeInfo a -> Int -> AlphaBeta -> MovePath
 minimaxHorizontal movePath [] _ _ _ = movePath
-minimaxHorizontal movePath (r:rs) (TreeInfo scoreF level True) indexM (AlphaBeta alpha beta) =
-    let treeInfo = (TreeInfo scoreF level True) in
-    let score = (Score movePath) in
+minimaxHorizontal movePath (r:rs) (TreeInfo scoreF nextNodesF level True) indexM (AlphaBeta alpha beta) =
+    let treeInfo = (TreeInfo scoreF nextNodesF level True) in
+    let score = (pathScore movePath) in
     if score > beta then movePath
     else let newAlpha = max score alpha in
         minimaxHorizontalAux movePath (r:rs) treeInfo indexM (AlphaBeta newAlpha beta)
-minimaxHorizontal movePath (r:rs) (TreeInfo scoreF level False) indexM (AlphaBeta alpha beta) =
-    let treeInfo = (TreeInfo scoreF level False) in
-    let score = (Score movePath) in
+minimaxHorizontal movePath (r:rs) (TreeInfo scoreF nextNodesF level False) indexM (AlphaBeta alpha beta) =
+    let treeInfo = (TreeInfo scoreF nextNodesF level False) in
+    let score = (pathScore movePath) in
     if score < alpha then movePath
     else let newBeta = min score beta in
         minimaxHorizontalAux movePath (r:rs) treeInfo indexM (AlphaBeta alpha newBeta)
 
-minimaxHorizontalAux :: MovePath -> [RoseTree a] -> TreeInfo a -> Int -> AlphaBeta -> MovePath
+minimaxHorizontalAux :: MovePath -> [a] -> TreeInfo a -> Int -> AlphaBeta -> MovePath
 minimaxHorizontalAux movePath (r:rs) treeInfo indexM ab =  minimaxHorizontal (bestMove (isMax treeInfo) movePath (nextLevelMinimax r treeInfo indexM ab)) rs treeInfo (indexM+1) ab
 
-nextLevelMinimax :: RoseTree a -> TreeInfo a -> Int -> AlphaBeta -> MovePath
-nextLevelMinimax r (TreeInfo scoreF level isMax) indexM ab = appendIndex (minimax r (TreeInfo scoreF (level-1) (not isMax)) indexM ab) indexM
+nextLevelMinimax :: a -> TreeInfo a -> Int -> AlphaBeta -> MovePath
+nextLevelMinimax r (TreeInfo scoreF nextNodesF level isMax) indexM ab = appendIndex (minimax r (TreeInfo scoreF nextNodesF (level-1) (not isMax)) indexM ab) indexM
 
 appendIndex :: MovePath -> Int -> MovePath
 appendIndex (MovePath score nExplored scoreSeen indexes) indexM = MovePath score (nExplored + 1) scoreSeen (indexM:indexes)
@@ -105,7 +116,7 @@ bestMove isMax (MovePath score1 nExplored1 scoreSeen1 idx1) (MovePath score2 nEx
     if comp then (MovePath score1 (nExplored1 + nExplored2) (scoreSeen1++scoreSeen2) idx1) else (MovePath score2 (nExplored1 + nExplored2) (scoreSeen1++scoreSeen2) idx2)
 
 freeMinimax :: MovePath
-freeMinimax = minimax freeTree (TreeInfo id 3 False) 0 (AlphaBeta (-1000000) 1000000)
+freeMinimax = minimax freeTree (TreeInfo getRoseTreeScore getRoseNextNodes 3 True) 0 (AlphaBeta (-1000000) 1000000)
 
 
 
@@ -127,7 +138,86 @@ countNodesAux (r:rs) = countNodes(r) + countNodesAux(rs)
 -------------------
 
 
+
+
+
+-- MinimaxAB
 --
+--minimax :: RoseTree a -> TreeInfo a -> Int -> AlphaBeta -> MovePath -- Tree, treeInfo, indexMove
+--minimax (Node a []) (TreeInfo scoreF _ _) indexM ab = MovePath (scoreF a) 0 [(scoreF a)] []
+--minimax (Node a _) (TreeInfo scoreF 0 _) indexM ab = MovePath (scoreF a) 0 [(scoreF a)] []
+--minimax (Node a (r:rs)) treeInfo indexM ab = minimaxHorizontal (nextLevelMinimax r treeInfo 0 ab) rs treeInfo 1 ab
+--
+--minimaxHorizontal :: MovePath -> [RoseTree a] -> TreeInfo a -> Int -> AlphaBeta -> MovePath
+--minimaxHorizontal movePath [] _ _ _ = movePath
+--minimaxHorizontal movePath (r:rs) (TreeInfo scoreF level True) indexM (AlphaBeta alpha beta) =
+--    let treeInfo = (TreeInfo scoreF level True) in
+--    let score = (pathScore movePath) in
+--    if score > beta then movePath
+--    else let newAlpha = max score alpha in
+--        minimaxHorizontalAux movePath (r:rs) treeInfo indexM (AlphaBeta newAlpha beta)
+--minimaxHorizontal movePath (r:rs) (TreeInfo scoreF level False) indexM (AlphaBeta alpha beta) =
+--    let treeInfo = (TreeInfo scoreF level False) in
+--    let score = (pathScore movePath) in
+--    if score < alpha then movePath
+--    else let newBeta = min score beta in
+--        minimaxHorizontalAux movePath (r:rs) treeInfo indexM (AlphaBeta alpha newBeta)
+--
+--minimaxHorizontalAux :: MovePath -> [RoseTree a] -> TreeInfo a -> Int -> AlphaBeta -> MovePath
+--minimaxHorizontalAux movePath (r:rs) treeInfo indexM ab =  minimaxHorizontal (bestMove (isMax treeInfo) movePath (nextLevelMinimax r treeInfo indexM ab)) rs treeInfo (indexM+1) ab
+--
+--nextLevelMinimax :: RoseTree a -> TreeInfo a -> Int -> AlphaBeta -> MovePath
+--nextLevelMinimax r (TreeInfo scoreF level isMax) indexM ab = appendIndex (minimax r (TreeInfo scoreF (level-1) (not isMax)) indexM ab) indexM
+--
+--appendIndex :: MovePath -> Int -> MovePath
+--appendIndex (MovePath score nExplored scoreSeen indexes) indexM = MovePath score (nExplored + 1) scoreSeen (indexM:indexes)
+--
+--bestMove :: Bool -> MovePath -> MovePath -> MovePath
+--bestMove isMax (MovePath score1 nExplored1 scoreSeen1 idx1) (MovePath score2 nExplored2 scoreSeen2 idx2) = let comp = if isMax then (score1 >= score2) else (score1 <= score2) in -- >= and <= because in case of tie we don't want to change
+--    if comp then (MovePath score1 (nExplored1 + nExplored2) (scoreSeen1++scoreSeen2) idx1) else (MovePath score2 (nExplored1 + nExplored2) (scoreSeen1++scoreSeen2) idx2)
+--
+--freeMinimax :: MovePath
+--freeMinimax = minimax freeTree (TreeInfo id 3 True) 0 (AlphaBeta (-1000000) 1000000)
+
+--
+--
+----- Other functions ---
+--
+----- Count nodes ---
+--
+--freeCount :: Int
+--freeCount = countNodes freeTree
+--
+--countNodes :: RoseTree a -> Int
+--countNodes (Node a []) = 1
+--countNodes (Node a (rs)) = 1 + countNodesAux(rs)
+--
+--countNodesAux :: [RoseTree a] -> Int
+--countNodesAux [] = 0
+--countNodesAux (r:rs) = countNodes(r) + countNodesAux(rs)
+--
+---------------------
+--
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- Minimax without AB
 --
 --minimax :: RoseTree a -> TreeInfo a -> Int -> MovePath -- Tree, treeInfo, indexMove
 --minimax (Node a []) (TreeInfo scoreF _ _) indexM = MovePath (scoreF a) []
