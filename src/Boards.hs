9fec415e0ffba401b983ddd358a134bada0cf9e6
Diego de Rochebouet <ddegrimaudetderocheb@itba.edu.ar>
2019-07-06 17:00:54 -0300

    Add pawn movement to list of movements

--- src/Boards.hs
+++ src/Boards.hs
@@ -32,8 +32,8 @@ testBoard = Board [
         Piece White (Rook False), Piece White (Knight    ), Piece White (Bishop    ), Piece White (Queen     ), Piece White (King False), Piece White (Bishop    ), Piece White (Knight    ), Piece White (Rook False),
         Piece White (Pawn False), Piece White (Pawn False), Piece White (Pawn False), Piece White (Pawn False), Piece White (Pawn False), Piece White (Pawn False), Piece White (Pawn False), Piece White (Pawn False), 
         None                    , Piece White (Pawn False), None                     , None                    , None                    , None                    , None                    , None                    ,
-        None                   , Piece White (Knight),   None                     , None                    , None                    , None                    , None                    , None                    ,
-        None                    , None                   , Piece Black (Pawn False) , None                    , None                    , None                    , None                    , None                    ,
+        None                   , Piece White (Pawn False),   None                     , None                    , None                    , None                    , None                    , None                    ,
+        Piece White (Pawn False) , Piece Black (Pawn False), Piece Black (Pawn False) , None                    , None                    , None                    , None                    , None                    ,
         None                    , None                   , Piece Black (Pawn False)  , None                    , None                    , None                    , None                    , None                    ,
         Piece Black (Pawn False), Piece Black (Pawn False), Piece Black (Pawn False), Piece Black (Pawn False), Piece Black (Pawn False), Piece Black (Pawn False), Piece Black (Pawn False), Piece Black (Pawn False), 
         Piece Black (Rook False), Piece Black (Knight    ), Piece Black (Bishop    ), Piece Black (Queen     ), Piece Black (King False), Piece Black (Bishop    ), Piece Black (Knight    ), Piece Black (Rook False)
@@ -103,7 +103,24 @@ listMoves index (Piece color Bishop) board = listMoveUsingDirection True index [
 listMoves index (Piece color Queen) board = listMoveUsingDirection True index [top, bottom, left, right, topRight, leftTop, bottomLeft, rightBottom] (moveType color board)
 listMoves index (Piece color (King hasMoved)) board = listMoveUsingDirection False index [top, bottom, left, right, topRight, leftTop, bottomLeft, rightBottom] (moveType color board)
 listMoves index (Piece color Knight) board = listMoveUsingDirection False index [knightBottomLeft, knightBottomRight, knightLeftBottom, knightLeftTop, knightRightBottom, knightRightTop, knightTopLeft, knightTopRight] (moveType color board)
-listMoves index _ board = [PiecePosition (-1) (-1)]
+listMoves index (Piece color (Pawn hasMoved)) board = listPawnMovement index color hasMoved board
+
+listPawnMovement :: PiecePosition -> Color -> Bool -> Board -> [PiecePosition]
+listPawnMovement index color hasMoved board = (pawnFrontMove index color hasMoved board) ++ (listPawnTakeMovement index color board)
+
+listPawnTakeMovement :: PiecePosition -> Color -> Board -> [PiecePosition]
+listPawnTakeMovement index color board = let takeMovements = if color == White then [leftTop, topRight] else [bottomLeft, rightBottom]
+                                         in listMoveUsingDirection False index takeMovements (moveTypeOnlyTakeAllowed color board)
+
+pawnFrontMove :: PiecePosition -> Color -> Bool -> Board -> [PiecePosition]
+pawnFrontMove index color hasMoved board = let frontMovement = if color == White then top else bottom
+                                           in let moveTypeF = moveTypeTakeNonAllowed board
+                                           in pawnFrontMoveAux frontMovement moveTypeF hasMoved (listMoveNonRecursive index frontMovement moveTypeF)
+
+pawnFrontMoveAux :: (PiecePosition -> Maybe PiecePosition) -> (PiecePosition -> MoveType) -> Bool -> [PiecePosition] -> [PiecePosition]
+pawnFrontMoveAux _ _ True positions = positions
+pawnFrontMoveAux _ _ _ [] = []
+pawnFrontMoveAux moveFunc moveTypeF hasMoved (pp:[]) = pp:(listMoveNonRecursive pp moveFunc moveTypeF)
 
 listMoveUsingDirection :: Bool -> PiecePosition -> [(PiecePosition -> Maybe PiecePosition)] -> (PiecePosition -> MoveType) -> [PiecePosition]
 listMoveUsingDirection _ _ [] _ = []
@@ -129,7 +146,17 @@ listMoveRecursive index moveFunc moveTypeF = case (moveFunc index) of
 moveType::Color -> Board -> PiecePosition -> MoveType
 moveType myColor board index = case (getPiece board index) of
   None -> SimpleMove
-  Piece otherColor pieceTypes -> if (otherColor == myColor) then InvalidMove else TakeMove 
+  Piece otherColor pieceTypes -> if (otherColor == myColor) then InvalidMove else TakeMove
+
+moveTypeTakeNonAllowed:: Board -> PiecePosition -> MoveType
+moveTypeTakeNonAllowed board index = case (getPiece board index) of
+  None -> SimpleMove
+  Piece _ _ -> InvalidMove
+
+moveTypeOnlyTakeAllowed::Color -> Board -> PiecePosition -> MoveType
+moveTypeOnlyTakeAllowed myColor board index = case (getPiece board index) of
+  None -> InvalidMove
+  Piece otherColor pieceTypes -> if (otherColor == myColor) then InvalidMove else TakeMove
 
 getPiece::Board -> PiecePosition -> Piece
 getPiece (Board pieces) (PiecePosition posX posY) = pieces !! (posX * 8 + posY)
